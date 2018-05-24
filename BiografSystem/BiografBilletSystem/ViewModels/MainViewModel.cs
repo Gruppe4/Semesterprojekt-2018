34c3e342a8049ca98ff444a439fe25ef6f75938b
Mads Meinert Andersen <mads485d@edu.easj.dk>
2018-05-24 12:28:17 +0200

    Libs

--- BiografSystem/BiografBilletSystem/ViewModels/MainViewModel.cs
+++ BiografSystem/BiografBilletSystem/ViewModels/MainViewModel.cs
@@ -1,11 +1,17 @@
-﻿using System.Windows.Input;
+﻿using System.ServiceModel;
+using System.Windows.Input;
 using Windows.UI.Xaml.Controls;
 using ViewModel.App.Implementation;
 
 namespace BiografBilletSystem.ViewModels
 {
-    class MainViewModel
+    class MainViewModel 
     {
-        
+        public object Server { get; private set; }
+
+        protected void Videre_Click(object sender, System.EventArgs e)
+        {
+            Server.Transfer("SalPage.xaml", true);
+        }
     }
 }
