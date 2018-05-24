f399ef7910771ca36273f7b7818779a137103fd8
Henrik Andersen <henr563a@edu.easj.dk>
2018-05-24 12:26:15 +0200

    RelayCommand
    RelayCommand

---
+++ BiografSystem/BiografBilletSystem/ViewModels/Commands/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace BiografBilletSystem.ViewModels.Commands
+{
+    public class RelayCommand<T> : ICommand
+    {
+        private Action _action;
+        private Func<bool> _canExecuteFunc;
+        
+        public RelayCommand(Action actionToExecute, Func<bool> canExecuteFunc)
+        {
+            _action = actionToExecute;
+            _canExecuteFunc = canExecuteFunc;
+        }
+        public RelayCommand(Action methodToExecute)
+            : this(methodToExecute, null) { }
+        public bool CanExecute(object parameter)
+        {
+            if (this._canExecuteFunc == null)
+            {
+                return true;
+            }
+            else
+            {
+                bool result = this._canExecuteFunc.Invoke();
+                return result;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
