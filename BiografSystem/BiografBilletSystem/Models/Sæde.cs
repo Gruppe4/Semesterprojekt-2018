31a3fc4e3c548156f94c65d694a6b76ea87303c9
Henrik Andersen <henr563a@edu.easj.dk>
2018-05-24 09:37:17 +0200

    Sæde

--- BiografSystem/BiografBilletSystem/Models/Sæde.cs
+++ BiografSystem/BiografBilletSystem/Models/Sæde.cs
@@ -6,11 +6,13 @@
 
         public int RækkeNr { get; private set; }
         public int Nummer { get; private set; }
+        public bool Reserveret { get; set; }
 
         public Sæde(int rækkeNr, int nummer)
         {
             RækkeNr = rækkeNr;
             Nummer = nummer;
+            Reserveret = false;
         }
     }
 }
