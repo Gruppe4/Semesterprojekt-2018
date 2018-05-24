d6e16ff1f3752da2ebd8ac119b989c19d58f3b09
Henrik Andersen <henr563a@edu.easj.dk>
2018-05-24 12:32:57 +0200

    Rettede Mads's fejl

--- BiografSystem/BiografBilletSystem/ViewModels/MainViewModel.cs
+++ BiografSystem/BiografBilletSystem/ViewModels/MainViewModel.cs
@@ -1,17 +1,7 @@
-﻿using System.ServiceModel;
-using System.Windows.Input;
-using Windows.UI.Xaml.Controls;
-using ViewModel.App.Implementation;
-
-namespace BiografBilletSystem.ViewModels
+﻿namespace BiografBilletSystem.ViewModels
 {
-    class MainViewModel 
+    class MainViewModel
     {
-        public object Server { get; private set; }
 
-        protected void Videre_Click(object sender, System.EventArgs e)
-        {
-            Server.Transfer("SalPage.xaml", true);
-        }
     }
 }
