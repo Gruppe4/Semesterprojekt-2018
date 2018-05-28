0beb74ed3328bf58807762e9af3328d2f0b17ca0
Mads Meinert Andersen <mads485d@edu.easj.dk>
2018-05-28 11:07:34 +0200

    Hej Marcel

--- BiografSystem/BiografBilletSystem/Models/Betaling.cs
+++ BiografSystem/BiografBilletSystem/Models/Betaling.cs
@@ -8,6 +8,18 @@ namespace BiografBilletSystem
 {
     class Betaling
     {
+        public int Kortnummer { get; set; }
+        public int Kontrolcifre { get; set; }
+        public DateTime Udløbsdato { get; set; }
+        public string Kortholder { get; set; }
+
+        public Betaling(int kortnummer, int kontrolcifre, DateTime udløbsdato, string kortholder)
+        {
+            Kortnummer = kortnummer;
+            Kontrolcifre = kontrolcifre;
+            Udløbsdato = udløbsdato;
+            Kortholder = kortholder;
+        }
 
     }
 }
