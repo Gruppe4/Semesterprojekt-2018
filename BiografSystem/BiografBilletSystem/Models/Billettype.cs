0ebf2fa83d8ca6dce4288364d993f0ce61720a21
Marcel Mølgaard Kristensen <marc1495@edu.easj.dk>
2018-05-28 12:15:12 +0200

    whattup

---
+++ BiografSystem/BiografBilletSystem/Models/Billettype.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiografBilletSystem.Models
+{
+    class Billettype
+    {
+
+        public int BørneBillet { get; private set; }
+
+        public int VoksenBillet { get; private set; }
+
+        public int SeniorBillet { get; private set; }
+
+        public int Medlemsskab { get; private set; }
+
+        public Billettype(int børneBillet, int voksenBillet, int seniorBillet, int medlemsskab)
+        {
+            BørneBillet = 70;
+            VoksenBillet = 100;
+            SeniorBillet = 70;
+            Medlemsskab = 80;
+        }
+    }
+}
